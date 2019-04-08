9dcbc89c9826e5b04deed5e67ab56013d54214ed
petern160 <peternguyen160@gmail.com>
2019-04-08 21:13:57 +0000

    added secret.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,25 @@
 var request = require('request');
+var secret = require('secret.js')
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization' : 'Bearer ' + secret.GITHUB_TOKEN
+      }
+      
+    };
+  
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
+  }
+getRepoContributors("lighthouse-labs", "assessment-exam-student", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
+
+  
